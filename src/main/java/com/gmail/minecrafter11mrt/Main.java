62894740aa72d5547af4eb864576d08f16c3f7c4
underscore11-wh <minecrafter11mrt@gmail.com>
2019-11-04 12:10:47 -0800

    save

--- src/main/java/com/gmail/minecrafter11mrt/Main.java
+++ src/main/java/com/gmail/minecrafter11mrt/Main.java
@@ -1,51 +1,8 @@
 package com.gmail.minecrafter11mrt;
-import org.javacord.api.DiscordApi;
-import org.javacord.api.DiscordApiBuilder;
-import org.javacord.api.entity.channel.TextChannel;
-import org.javacord.api.entity.user.User;
-
-import java.io.File;
-import java.util.logging.ConsoleHandler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import com.esotericsoftware.*;
-
 public class Main {
-    static String ver = "v0.5.1";
-    static DiscordApi api;
-    static TextChannel logchannel;
-    static User botowner;
-    private static Logger logger = Logger.getLogger("Main");
-    private static Level loglevel=Level.FINEST;
     public static void main(String[] args) {
-        Main.initLogger(logger);
-        logger.log(Level.INFO,"Logging in with token "+args[0]);
-        api = new DiscordApiBuilder().setToken(args[0]).login().join();
-        logger.log(Level.INFO,"Logged in!");
-        logger.log(Level.INFO,"Running Exambot "+ver);
-        api.getTextChannelById(638474520654512128L).ifPresent(textChannel ->{
-            logchannel=textChannel;
-            logger.log(Level.FINE,"Logchannel initialized",logchannel);
-        });
-        api.getOwner().thenAccept(owner ->{
-            botowner=owner;
-            logger.log(Level.FINE,"Botowner intialized",botowner);
-        });
-        api.addListener(new MessageListener());
-        api.updateActivity("-help | "+ver);
-    }
-    static void initLogger(Logger otherLogger){
-        otherLogger.setLevel(loglevel);
-        ConsoleHandler handler = new ConsoleHandler();
-        handler.setLevel(Main.loglevel);
-        if(otherLogger.getHandlers().length==0) {
-            otherLogger.addHandler(handler);
-        }
-        //System.out.println("Active handlers for "+otherLogger.getName()+": "+otherLogger.getHandlers().length);
-    }
-    static void shutdown(){
-        api.disconnect();
-        System.exit(0);
+        System.out.println("Hey!");
+        System.out.println("I'm a spigot plugin");
+        System.out.println("Don't run me directly, put me in your plugin folder");
     }
 }
---
+++ src/main/java/com/gmail/minecrafter11mrt/WHBot.java
@@ -0,0 +1,41 @@
+package com.gmail.minecrafter11mrt;
+
+import org.bukkit.plugin.PluginLogger;
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.entity.channel.TextChannel;
+import org.javacord.api.entity.user.User;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class WHBot extends JavaPlugin {
+    static String ver = "v0.1.1";
+    static DiscordApi api;
+    static User botowner;
+    static Logger logger;
+
+    @Override
+    public void onEnable() {
+        logger=getLogger();
+        this.saveDefaultConfig();
+        String token=this.getConfig().getString("token");
+        logger.log(Level.INFO,"Logging in with token "+token);
+        api = new DiscordApiBuilder().setToken(token).login().join();
+        logger.log(Level.INFO,"Logged in!");
+        logger.log(Level.INFO,"Running WHBot "+ver);
+        api.getOwner().thenAccept(owner ->{
+            botowner=owner;
+            logger.log(Level.FINE,"Botowner intialized",botowner);
+        });
+        api.addListener(new MessageListener());
+        api.updateActivity("-help | "+ver);
+    }
+    @Override
+    public void onDisable() {
+        api.disconnect();
+        logger.log(Level.INFO,"Disconnected from Discord");
+    }
+}
